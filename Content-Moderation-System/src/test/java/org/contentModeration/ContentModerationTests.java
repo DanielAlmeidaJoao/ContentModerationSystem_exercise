c4aa700f6106108f357b59286ea9d2bf601d70b6
Daniel de Almeida <d.joao@campus.fct.unl.pt>
2024-12-29 12:59:44 +0000

    Adds surefire plugin to run junit 5 tests

--- Content-Moderation-System/src/test/java/org/contentModeration/ContentModerationTests.java
+++ Content-Moderation-System/src/test/java/org/contentModeration/ContentModerationTests.java
@@ -14,7 +14,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class TestContentModeration {
+class ContentModerationTests {
     // PROGRAM EXECUTION
     TranslationService translationService = mockTranslationService();
     ScoringService scoringService = mockScoringService();
